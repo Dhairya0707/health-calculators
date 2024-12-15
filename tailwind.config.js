4dd1cf30d151c7fcd289610181e92117bb8ef6d4
Dhairya <118837648+Dhairya0707@users.noreply.github.com>
2024-12-15 16:24:34 +0530

    Describe your changes

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [   
+      "dark", 
+      "retro",
+      "halloween",
+      "lofi",
+      "dracula",
+      "forest",
+      "fantasy",
+      "black",
+      "acid",
+      "winter"
+     ],
+  },
+}
